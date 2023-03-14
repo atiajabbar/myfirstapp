8a5e41a49d1f21854adf0973ac6a448bd02e6e29
Atia Khan <atiajabbar39@gmail.com>
2023-03-15 01:21:30 +0500

    first react updated repo

---
+++ src/components/Message.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+// const Message = () =>{ 
+//       return (
+//         <div classame = "ui fixed menu"> 
+//       <div className='ui container center'>
+//         <h2>Hello World! </h2></div>
+//         </div>
+//       );
+//   };
+  class Message extends React.Component {
+    render() {
+      return <div className='ui container center'>
+        <h1>Hello Atia Khan</h1>
+        </div>;
+    //   <div>Hello World! </div>
+    }
+  }
+//   root.render(<Message name="Atia Khan" />);
+
+  export default Message;

3a9853a6c154a7de1b045abaf8620049c05d0c71
AJkhan <atiajabbar39@gmail.com>
2023-03-18 13:22:39 +0500

    Update Message.js

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
   const Message() {
-    render() {
-      return 
-      <div
+
+      return (
+      <div>
         <h1>Hello Atia Khan</h1>
         </div>
-  
+  );
+  }
 
   export default Message;

caa9b92bde0c3b75be2d0d661400ebd716548b80
AJkhan <atiajabbar39@gmail.com>
2023-03-18 11:51:06 +0500

    Message Component Updated
    Message component updated from class method to function method

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,21 +1,11 @@
 import React, { Component } from 'react';
 
-// const Message = () =>{ 
-//       return (
-//         <div classame = "ui fixed menu"> 
-//       <div className='ui container center'>
-//         <h2>Hello World! </h2></div>
-//         </div>
-//       );
-//   };
-  class Message extends React.Component {
+  const Message() {
     render() {
-      return <div className='ui container center'>
+      return 
+      <div
         <h1>Hello Atia Khan</h1>
-        </div>;
-    //   <div>Hello World! </div>
-    }
-  }
-//   root.render(<Message name="Atia Khan" />);
+        </div>
+  
 
   export default Message;
